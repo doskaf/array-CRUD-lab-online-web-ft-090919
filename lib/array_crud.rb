6c10b39719f6d240c4fce3b4970c361bb766559d
Dominique Skaf <doskaf@yahoo.com>
2019-09-11 16:38:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [1,2]
+  array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
